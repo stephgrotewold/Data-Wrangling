e9248e6b4317430b733ef6e2d86902e7c33e96b6
Steph <79061296+stephgrotewold@users.noreply.github.com>
2023-10-01 22:38:19 -0600

    Update lab5.R

--- Lab#5/lab5.R
+++ Lab#5/lab5.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(ggplot2)
 library(readxl)
 library(stringr)
 library(lubridate)
@@ -6,26 +7,22 @@ library(nycflights13)
 
 
 #----*Parte 1: Predecir un eclipse solar*----
-# Fecha del eclipse histórico en la zona horaria de América del Norte (por ejemplo, Nueva York)
 fecha_eclipse_historico <- with_tz(ymd_hms("2017-08-21 18:26:40"), tzone = "America/New_York")
 
-# Duración de un Synodic Month
+#Duración de un Synodic Month
 synodic_month <- ddays(29) + dhours(12) + dminutes(44) + dseconds(3)
 
-# Duración de un Saros (223 Synodic Months)
+#Duración de un Saros (223 Synodic Months)
 saros <- synodic_month * 223
 
-# Calcular la fecha del siguiente eclipse solar en la zona horaria de América del Norte
+#Calcular la fecha del siguiente eclipse solar en la zona horaria de América del Norte
 fecha_siguiente_eclipse <- fecha_eclipse_historico + saros
-
-# Imprimir la fecha del siguiente eclipse solar en la zona horaria de América del Norte
-print(fecha_siguiente_eclipse)
-
-
+fecha_siguiente_eclipse
 
 
 #----*Parte 2: Agrupaciones y Operaciones con Fechas*----
 data_df <- read_excel("data.xlsx")
+str(data_df)
 data_df2 <- data_df %>%
   rename(
     Fecha_Creacion = "Fecha Creación",
@@ -33,16 +30,93 @@ data_df2 <- data_df %>%
     Hora_Creacion = "Hora Creación",
     Hora_Final = "Hora Final"
   )
-head(data_df2$Fecha_Creacion)
+head(data_df2$Fecha_Creacion,10)
+
+data_df2 <- data_df2 %>% 
+  mutate(
+    Fecha_Creacion_Temp = Fecha_Creacion,
+    Fecha_Creacion = dmy(Fecha_Creacion),
+    Fecha_Creacion_Temp = as.Date(as.numeric(Fecha_Creacion_Temp), origin = "1899-12-30"),
+    Fecha_Final_Temp = Fecha_Final,
+    Fecha_Final = dmy(Fecha_Final),
+    Fecha_Final_Temp = as.Date(as.numeric(Fecha_Final_Temp), origin = "1899-12-30")
+  )
+
+data_df2 <- data_df2 %>% 
+  mutate(
+    Fecha_Creacion_Final = coalesce(Fecha_Creacion_Temp, Fecha_Creacion),
+    Fecha_Final_Finalito = coalesce(Fecha_Final_Temp, Fecha_Final)
+  )
+ 
+str(data_df2)
+
+
+#1. ¿En qué meses existe una mayor cantidad de llamadas por código?
+meses_mas_llamadas <- data_df2 %>%
+  mutate(Month = month(Fecha_Creacion_Final)) %>%
+  group_by(Month, Cod) %>%
+  summarise(Total_Llamadas = n()) %>%
+  arrange(desc(Total_Llamadas))
+meses_mas_llamadas
+
+
+#2. ¿Qué día de la semana es el más ocupado?
+dia_semana_mas_ocupado <- data_df2 %>%
+  filter(!is.na(Fecha_Creacion_Final)) %>%
+  mutate(DayOfWeek = weekdays(Fecha_Creacion_Final)) %>%
+  group_by(DayOfWeek) %>%
+  summarise(Total_Llamadas = n()) %>%
+  arrange(desc(Total_Llamadas))
+dia_semana_mas_ocupado
+
+
+#3. ¿Qué mes es el más ocupado?
+mes_mas_ocupado <- data_df2 %>%
+  filter(!is.na(Fecha_Creacion_Final)) %>%
+  mutate(Month = month(Fecha_Creacion_Final)) %>%
+  group_by(Month) %>%
+  summarise(Total_Llamadas = n()) %>%
+  arrange(desc(Total_Llamadas))
+mes_mas_ocupado
+
+#4. ¿Existe una concentración o estacionalidad en la cantidad de llamadas?
+llamadas_por_fecha <- data_df2 %>%
+  filter(!is.na(Fecha_Creacion_Final)) %>%
+  group_by(Fecha_Creacion_Final) %>%
+  summarise(Total_Llamadas = n())
+
+ggplot(llamadas_por_fecha, aes(x = Total_Llamadas)) +
+  geom_histogram(binwidth = 1, fill = "pink", color = "black") +
+  labs(
+    title = "Histograma de la Cantidad de Llamadas por Fecha",
+    x = "Cantidad de Llamadas",
+    y = "Frecuencia"
+  )
+
+#5. ¿Cuántos minutos dura la llamada promedio?
+head(data_df2$Hora_Creacion)
+
+data_df2$Hora_Creacion <- as.POSIXct(data_df2$Hora_Creacion, format = "%H:%M:%S")
+data_df2$Hora_Final <- as.POSIXct(data_df2$Hora_Final, format = "%H:%M:%S")
+
+data_df2$Duracion <- as.numeric(difftime(data_df2$Hora_Final, data_df2$Hora_Creacion, units = "mins"))
+
+duracion_promedio <- mean(data_df2$Duracion, na.rm = TRUE)
+cat("La duración promedio es de:", duracion_promedio, "minutos\n")
+
+#6. Realice una tabla de frecuencias con el tiempo de llamada.
+tabla_frecuencias <- table(data_df2$Duracion)
+tabla_frecuencias
+
 
 #----* Parte 3: Signo Zodiacal*----
 zodiac <- function(año, mes, dia) {
-  # Verificar si el año es bisiesto
+  #Verificar si el año es bisiesto
   es_bisiesto <- lubridate::leap_year(año)
   
-  # Verificar si la fecha es válida
+  #Verificar si la fecha es válida
   if ((mes >= 1 && mes <= 12) && (dia >= 1 && dia <= 31)) {
-    # Verificar si la fecha es válida para febrero
+    #Verificar si la fecha es válida para febrero
     if (mes == 2) {
       if ((dia >= 1 && dia <= 29 && es_bisiesto) || (dia >= 1 && dia <= 28 && !es_bisiesto)) {
         return("Tu signo zodiacal es: Piscis")
@@ -51,7 +125,7 @@ zodiac <- function(año, mes, dia) {
       }
     }
     
-    # Resto de los meses
+  
     if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
       return("Tu signo zodiacal es: Aries")
     } else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
@@ -82,7 +156,6 @@ zodiac <- function(año, mes, dia) {
   }
 }
 
-# Ejemplo de uso:
 año_nacimiento <- 2000
 mes_nacimiento <- 2
 dia_nacimiento <- 25
@@ -91,23 +164,23 @@ cat(resultado, "\n")
 
 
 #----* Parte 4: Flights*----
-View(flights)
+#View(flights)
 flights2 <- flights
 
 
-View(flights)
+#View(flights)
 flights2 <- flights %>%
   mutate(
-    # Columna para dep_time en formato fecha y hora
+    #Columna para dep_time en formato fecha y hora
     dep_datetime = make_datetime(year, month, day, dep_time %/% 100, dep_time %% 100),
     
-    # Columna para arr_time en formato fecha y hora
+    #Columna para arr_time en formato fecha y hora
     arr_datetime = make_datetime(year, month, day, arr_time %/% 100, arr_time %% 100),
     
-    # Columna para sched_dep_time en formato fecha y hora
+    #Columna para sched_dep_time en formato fecha y hora
     sched_dep_datetime = make_datetime(year, month, day, sched_dep_time %/% 100, sched_dep_time %% 100),
     
-    # Columna para sched_arr_time en formato fecha y hora
+    #Columna para sched_arr_time en formato fecha y hora
     sched_arr_datetime = make_datetime(year, month, day, sched_arr_time %/% 100, sched_arr_time %% 100)
   )
 head(flights)
